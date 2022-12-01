8ec0c857a53badbf4635695dd9b2cb2844b0f82d
Daron Tan <s10242370@connect.np.edu.sg>
2022-12-01 15:17:07 +0800

    Wk7 removed start button (unnecessary)

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-function startButton() {
-    var x = document.getElementById("hidden");
-    var y = document.getElementById("start");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-        y.style.display = "none";
-    }
-
-    else {
-        x.style.display = "none"; 
-    }
-}
-
-const button = document.getElementById('start');
-
-
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
