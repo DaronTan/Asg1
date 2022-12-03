f680d9d23eb050adce04355f4c4526d941e3dde5
Daron Tan <s10242370@connect.np.edu.sg>
2022-12-04 00:15:28 +0800

    Wk7 added css for popup hide and show feature

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function openTab(tabname) {
-    for(link of tablinks) {
-        link.classList.remove("active-link");
-    }
-    for(content of tabcontents) {
-        content.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
+	for (link of tablinks) {
+		link.classList.remove("active-link");
+	}
+	for (content of tabcontents) {
+		content.classList.remove("active-tab");
+	}
+	event.currentTarget.classList.add("active-link");
+	document.getElementById(tabname).classList.add("active-tab");
 }
 
-window.addEventListener('scroll', () => {
-    let header = document.querySelector('.container');
-    header.classList.toggle('sticky', window.scrollY > 0);
-
-})
+window.addEventListener("scroll", () => {
+	let header = document.querySelector(".container");
+	header.classList.toggle("sticky", window.scrollY > 0);
+});
